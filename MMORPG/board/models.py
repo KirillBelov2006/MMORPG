2c6f4c7435a4bb7db31c8130ab01ad9c6018cb6a
Kirill_Belov <126522169+KirillBelov2006@users.noreply.github.com>
2024-04-13 13:15:16 +0300

    Update models.py

--- MMORPG/board/models.py
+++ MMORPG/board/models.py
@@ -5,7 +5,7 @@ from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    CAT = (('tanks', 'Танки'),
+    CATEGORIES = (('tanks', 'Танки'),
            ('healers', 'Хилы'),
            ('damage_dealers', 'ДД'),
            ('dealers', 'Торговцы'),
@@ -15,7 +15,7 @@ class Post(models.Model):
            ('tanners', 'Кожевники'),
            ('potion_makers', 'Зельевары'),
            ('spell_masters', 'Мастера заклинаний'))
-    category = models.CharField(max_length=15, choices=CAT, verbose_name='Категория')
+    category = models.CharField(max_length=15, choices=CATEGORIES, verbose_name='Категория')
     dateCreation = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=256, verbose_name='Название')
     text = RichTextField()
