5e3496efe5d46e9edf903c78e2b26a52547f95d9
Kirill_Belov <126522169+KirillBelov2006@users.noreply.github.com>
2024-04-25 18:19:16 +0300

    Update views.py

--- MMORPG/board/views.py
+++ MMORPG/board/views.py
@@ -6,19 +6,19 @@ from django.shortcuts import redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
-from .models import Post, Response
+from .models import Announcement, Response
 from .forms import PostForm, RespondForm, ResponsesFilterForm
 from .tasks import respond_send_email, respond_accept_send_email
 
 
 class Index(ListView):
-    model = Post
+    model = Announcement
     template_name = 'index.html'
     context_object_name = 'posts'
 
 
 class PostItem(DetailView):
-    model = Post
+    model = Announcement
     template_name = 'post_item.html'
     context_object_name = 'post'
 
@@ -26,13 +26,13 @@ class PostItem(DetailView):
         context = super().get_context_data(**kwargs)
         if Response.objects.filter(author_id=self.request.user.id).filter(post_id=self.kwargs.get('pk')):
             context['respond'] = "Откликнулся"
-        elif self.request.user == Post.objects.get(pk=self.kwargs.get('pk')).author:
+        elif self.request.user == Announcement.objects.get(pk=self.kwargs.get('pk')).author:
             context['respond'] = "Мое_объявление"
         return context
 
 
 class CreatePost(LoginRequiredMixin, CreateView):
-    model = Post
+    model = Announcement
     template_name = 'create_post.html'
     form_class = PostForm
 
@@ -59,15 +59,15 @@ class EditPost(PermissionRequiredMixin, UpdateView):
     success_url = '/create/'
 
     def dispatch(self, request, *args, **kwargs):
-        author = Post.objects.get(pk=self.kwargs.get('pk')).author.username
+        author = Announcement.objects.get(pk=self.kwargs.get('pk')).author.username
         if self.request.user.username == 'admin' or self.request.user.username == author:
             return super().dispatch(request, *args, **kwargs)
         else:
-            return HttpResponse("Редактировать объявление может только его автор")
+            return HttpResponse("Редактировать запись может только автор")
 
     def get_object(self, **kwargs):
         id = self.kwargs.get('pk')
-        return Post.objects.get(pk=id)
+        return Announcement.objects.get(pk=id)
 
     def form_valid(self, form):
         form.save()
@@ -77,15 +77,15 @@ class EditPost(PermissionRequiredMixin, UpdateView):
 class DeletePost(PermissionRequiredMixin, DeleteView):
     permission_required = 'board.delete_post'
     template_name = 'delete_post.html'
-    queryset = Post.objects.all()
+    queryset = Announcement.objects.all()
     success_url = '/index'
 
     def dispatch(self, request, *args, **kwargs):
-        author = Post.objects.get(pk=self.kwargs.get('pk')).author.username
+        author = Announcement.objects.get(pk=self.kwargs.get('pk')).author.username
         if self.request.user.username == 'admin' or self.request.user.username == author:
             return super().dispatch(request, *args, **kwargs)
         else:
-            return HttpResponse("Удалить объявление может только его автор")
+            return HttpResponse("Удалить запись может только автор")
 
 
 title = str("")
@@ -99,31 +99,24 @@ class Responses(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(Responses, self).get_context_data(**kwargs)
         global title
-        """
-        Далее в условии - если пользователь попал на страницу через ссылку из письма, в которой содержится
-        ID поста для фильтра - фильтр работает по этому ID
-        """
-        if self.kwargs.get('pk') and Post.objects.filter(id=self.kwargs.get('pk')).exists():
-            title = str(Post.objects.get(id=self.kwargs.get('pk')).title)
+        if self.kwargs.get('pk') and Announcement.objects.filter(id=self.kwargs.get('pk')).exists():
+            title = str(Announcement.objects.get(id=self.kwargs.get('pk')).title)
             print(title)
         context['form'] = ResponsesFilterForm(self.request.user, initial={'title': title})
         context['title'] = title
         if title:
-            post_id = Post.objects.get(title=title)
+            post_id = Announcement.objects.get(title=title)
             context['filter_responses'] = list(Response.objects.filter(post_id=post_id).order_by('-dateCreation'))
             context['response_post_id'] = post_id.id
         else:
-            context['filter_responses'] = list(Response.objects.filter(post_id__author_id=self.request.user).order_by('-dateCreation'))
+            context['filter_responses'] = list(
+                Response.objects.filter(post_id__author_id=self.request.user).order_by('-dateCreation'))
         context['myresponses'] = list(Response.objects.filter(author_id=self.request.user).order_by('-dateCreation'))
         return context
 
     def post(self, request, *args, **kwargs):
         global title
         title = self.request.POST.get('title')
-        """
-        Далее в условии - При событии POST (если в пути открытой страницы есть ID) - нужно перезайти уже без этого ID
-        чтобы фильтр отрабатывал запрос уже из формы, так как ID, если он есть - приоритетный 
-        """
         if self.kwargs.get('pk'):
             return HttpResponseRedirect('/responses')
         return self.get(request, *args, **kwargs)
@@ -163,9 +156,7 @@ class Respond(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         respond = form.save(commit=False)
         respond.author = User.objects.get(id=self.request.user.id)
-        respond.post = Post.objects.get(id=self.kwargs.get('pk'))
+        respond.post = Announcement.objects.get(id=self.kwargs.get('pk'))
         respond.save()
         respond_send_email.delay(respond_id=respond.id)
         return redirect(f'/post/{self.kwargs.get("pk")}')
-
-
